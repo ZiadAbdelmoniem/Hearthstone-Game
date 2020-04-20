b110c61599b866f107fcda7c113ad40f920f3ae3
ZiadAbdelmoniem <ZiadAbdelmoniem@users.noreply.github.com>
2020-04-20 03:28:53 +0200

    yala bina ana fehemt el leila

--- Milestone 1/src/engine/GameController.java
+++ Milestone 1/src/engine/GameController.java
@@ -3,7 +3,11 @@ package engine;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import model.heroes.Hero;
+import model.heroes.Mage;
 
 public class GameController implements ActionListener,GameListener {
 	private Game model;
@@ -35,8 +39,19 @@ public class GameController implements ActionListener,GameListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getActionCommand().equals("chosen the heros lets play")){
+			JFrame game=new JFrame();
+			game.setBounds(600, 600, 600, 600);
+			JLabel text= new JLabel("the GAME has started");
+			game.add(text);
+			game.setVisible(true);
+			//Game g=new Game(firstplayer, secplayer);
+		}
+		/*if(e.getActionCommand().equals("chose mage 1")){
+			Mage m=new Mage();
+			firstplayer=(Hero) m;
+			
+		}*/
 	}
 	
 
