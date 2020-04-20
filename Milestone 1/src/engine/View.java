d1329c008d03ff931df26c018bc86086e5c2efea
ZiadAbdelmoniem <ZiadAbdelmoniem@users.noreply.github.com>
2020-04-20 03:33:16 +0200

    Update View.java

--- Milestone 1/src/engine/View.java
+++ Milestone 1/src/engine/View.java
@@ -1,14 +1,38 @@
 package engine;
 import java.awt.*;
-
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
-public class View extends JFrame {
+public class View extends JFrame implements GameListener {
+	public View(){
+		super();
+		this.setVisible(true);
+		this.setBounds(800, 800, 800, 800);
+		JPanel p = new JPanel();
+	    JLabel t = new JLabel("Player one");
+	    t.setFont(new Font("Verdana",1,20));
+	    p.add(t);
+	    p.setBorder(new LineBorder(Color.BLACK)); // make it easy to see
+		this.add(p);
+	    JButton mage= new JButton("Mage");
+		JButton priest= new JButton("Priest");
+		JButton hunter= new JButton("Hunter");
+		JButton paladin= new JButton("Paladin");
+		JButton warlock= new JButton("Warlock");
+		this.add(mage);
+		this.add(priest);
+		this.add(hunter);
+		this.add(paladin);
+		this.add(warlock);
+		//this.getContentPane().add(BorderLayout.CENTER,mage)
+		//maybe use this to control heteb2a feen el buttons
+		// this mainly says en awel ma el le3ba hetefta7 di awel 7aga heyshofoha 
+	}
 	
-	 public static void main(String[]args){
-	    	JFrame mainframe=new JFrame("boody");
-	    	mainframe.setVisible(true);
-	    	mainframe.setBounds(200, 200, 200, 200);
-	    	mainframe.getContentPane().setBackground(Color.MAGENTA);
-	 }
+	@Override
+	public void onGameOver() {
+		// TODO Auto-generated method stub
+	}
+		
+	
 }
